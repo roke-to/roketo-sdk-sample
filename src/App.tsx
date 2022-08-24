b125f81212d31cc6ff76535f9d307d18a11eccfb
Serge K Lebedev <hello@sergelebe.dev>
2022-08-24 19:52:18 +0600

    Add cat images and switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import logo from './logo.svg';
+import defaultCat from './img/defaultCat.png';
+import fedCat from './img/fedCat.png';
+import hungryCat from './img/hungryCat.png';
+
 import './App.css';
 
+const IS_FED = true;
+
 function App() {
+  const [catImage, setCatImage] = useState(defaultCat);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setCatImage((currentCat) => {
+      if (currentCat !== defaultCat) {
+        return defaultCat;
+      } else {
+        return IS_FED ? fedCat : hungryCat;
+      }
+    }), 2000);
+
+    return () => clearInterval(intervalId);
+  })
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={catImage} className="cat" alt="cat" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
